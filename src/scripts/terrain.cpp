ca33121aec7415971f14ac96a318807bbba5bcf8
ItouKotaro <231019@st.yoshida-g.ac.jp>
2024-11-15 22:58:28 +0900

    画像の書き出し処理を削除しました

--- src/scripts/terrain.cpp
+++ src/scripts/terrain.cpp
@@ -130,29 +130,6 @@ void CTerrain::GenerateTerrain()
 	heightMapBuilder.SetBounds(2.0, 6.0, 1.0, 5.0);
 	heightMapBuilder.Build();
 
-	utils::RendererImage renderer;
-	utils::Image image;
-	renderer.SetSourceNoiseMap(heightMap);
-	renderer.SetDestImage(image);
-	renderer.ClearGradient();
-	renderer.AddGradientPoint(-1.0000, utils::Color(0, 0, 128, 255)); // deeps
-	renderer.AddGradientPoint(-0.2500, utils::Color(0, 0, 255, 255)); // shallow
-	renderer.AddGradientPoint(0.0000, utils::Color(0, 128, 255, 255)); // shore
-	renderer.AddGradientPoint(0.0625, utils::Color(240, 240, 64, 255)); // sand
-	renderer.AddGradientPoint(0.1250, utils::Color(32, 160, 0, 255)); // grass
-	renderer.AddGradientPoint(0.3750, utils::Color(224, 224, 0, 255)); // dirt
-	renderer.AddGradientPoint(0.7500, utils::Color(128, 128, 128, 255)); // rock
-	renderer.AddGradientPoint(1.0000, utils::Color(255, 255, 255, 255)); // snow
-	renderer.EnableLight();
-	renderer.SetLightContrast(3.0);
-	renderer.SetLightBrightness(2.0); // Double the brightness
-	renderer.Render();
-
-	utils::WriterBMP writer;
-	writer.SetSourceImage(image);
-	writer.SetDestFilename("tutorial.bmp");
-	writer.WriteDestFile();
-
 	// 山道を生成する
 	GenerateRoad();
 
@@ -180,10 +157,6 @@ void CTerrain::GenerateTerrain()
 			m_pField->GetComponent<CMeshField>()->SetColor(x, y, GetVertexColor(x, y));
 		}
 	}
-
-	// テクスチャを生成する
-	//m_pField->GetComponent<CMeshField>()->SetTexture("data\\TEXTURE\\ground.png");
-	//m_pField->GetComponent<CMeshField>()->SetLoopTexture(100);
 
 	m_pField->GetComponent<CMeshField>()->ResetNormals();
 }
