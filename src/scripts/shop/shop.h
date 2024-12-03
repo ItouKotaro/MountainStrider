b5ba1575c042d97d7c7e174592ad075ef46cdcba
ItouKotaro <231019@st.yoshida-g.ac.jp>
2024-12-03 15:40:29 +0900

    ショップを作ろうとしました

---
+++ src/scripts/shop/shop.h
@@ -0,0 +1,23 @@
+//=============================================================
+//
+// ショップ [shop.h]
+// Author: Ito Kotaro
+// 
+// コインを使用して購入できる
+//
+//=============================================================
+#ifndef _SHOP_H_
+#define _SHOP_H_
+
+// ショップ管理クラス
+class ShopManager
+{
+public:
+	void Init();
+	void Uninit();
+	void Update();
+private:
+
+};
+
+#endif // !_SHOP_H_
