b7698bd506d1b6272306caa3449a570a45399498
ItouKotaro <231019@st.yoshida-g.ac.jp>
2024-12-16 15:31:13 +0900

    道のプログラムファイルを追加しました

---
+++ src/scripts/road.h
@@ -0,0 +1,22 @@
+//=============================================================
+//
+// “¹ [road.h]
+// Author: Ito Kotaro
+//
+//=============================================================
+#ifndef _ROAD_H_
+#define _ROAD_H_
+
+#include "component.h"
+//
+//class RoadCreator : public Component
+//{
+//public:
+//	void Init() override;
+//	void Uninit() override;
+//	
+//private:
+//
+//};
+
+#endif // !_ROAD_H_
