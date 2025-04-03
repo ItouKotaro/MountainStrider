7eb23e7d53f78a42c79c4a54ff53595363452c43
ItouKotaro <231019@st.yoshida-g.ac.jp>
2025-04-04 05:03:06 +0900

    定数のコメントを追加しました

--- src/scripts/vehicle_particle.h
+++ src/scripts/vehicle_particle.h
@@ -28,23 +28,23 @@ private:
 	//@brief 後方パーティクルの更新
 	void UpdateBackParticle();
 
-	CVehicle* m_vehicle;
-	Terrain* m_terrain;
-	LakeManager* m_lake;
-	GameObject* m_trajectory;
+	CVehicle* m_vehicle;				// バイク
+	Terrain* m_terrain;					// 地形
+	LakeManager* m_lake;			// 池
+	GameObject* m_trajectory;	// 軌道
+	GameObject* m_exhaust;		// 排気
 	float m_backParticleTimer;
 
 	// 定数
-	const float BP_MIN_GENERATE = 15.0f;
-	const float BP_RANDOM_TIME = 0.1f;
-	const float BP_RANDOM_WIDTH = 2.0f;
-	const float BP_BACK_RANGE = 10.0f;
-	const float BP_MIN_SIZE = 0.5f;
-	const float BP_SPEED_RATE = 0.01f;
-	const float BP_EXPANSE_VALUE = 5.0f;
-	const float BP_GRAVITY = -80.0f;
-	const float BG_LAKE_SPACE = 20.0f;
-	const float BG_LAKE_TOP = 10.0f;
+	const float BP_MIN_GENERATE = 15.0f;		// 最小生成
+	const float BP_RANDOM_TIME = 0.1f;		// 生成時間
+	const float BP_RANDOM_WIDTH = 2.0f;	// 横幅
+	const float BP_BACK_RANGE = 10.0f;		// 後方範囲
+	const float BP_MIN_SIZE = 0.5f;					// パーティクルの最小サイズ
+	const float BP_SPEED_RATE = 0.01f;			// 速さによるレート
+	const float BP_EXPANSE_VALUE = 5.0f;		// 広がり
+	const float BP_GRAVITY = -80.0f;				// 重力
+	const float BG_LAKE_SPACE = 20.0f;			// 池範囲の余分
 };
 
 #endif // !_VEHICLE_PARTICLE_H_
