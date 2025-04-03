656c176fb77448a4af5b5c934a023a1662797010
ItouKotaro <231019@st.yoshida-g.ac.jp>
2025-04-04 06:09:39 +0900

    デリバリーモードの追加

---
+++ src/scripts/mode/delivery.cpp
@@ -0,0 +1,21 @@
+//=============================================================
+//
+// デリバリーモード [delivery.cpp]
+// Author: Ito Kotaro
+// 
+// 配達モード
+//
+//=============================================================
+#include "delivery.h"
+
+void DeliveryMode::Init()
+{
+}
+
+void DeliveryMode::Uninit()
+{
+}
+
+void DeliveryMode::Update()
+{
+}
